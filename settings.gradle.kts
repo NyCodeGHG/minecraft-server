2b54b63fd2c88f5942ce5f57f59605bc85d79481
NyCodeGHG <nycode@protonmail.com>
2020-11-14 15:39:47 +0100

    Added ci env flag

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
 rootProject.name = "minecraft-server"
 
+val isCiServer = System.getenv().containsKey("CI")
+// Cache build artifacts, so expensive operations do not need to be re-computed
+buildCache {
+    local {
+        isEnabled = !isCiServer
+    }
+}
